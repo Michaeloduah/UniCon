76592047b259053097d181fae2ea1b4748f53999
Michael Oduah <oduahmichaelebuka@gmail.com>
2023-11-26 19:34:34 +0100

    Created Admin dashboard

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,10 +1,11 @@
-const { User } = require("../models");
+const bcrypt = require("bcrypt");
+const { Admin } = require("../models");
 
 async function index(req, res, next) {
   try {
     const user = req.session.user;
 
-    res.render("admin/index", {
+    res.render("admin/dashboard/index", {
       user: user,
       title: "Dashboard",
       breadcrumb: "Home",
@@ -18,7 +19,7 @@ async function profile(req, res) {
   try {
     const user = req.session.user;
 
-    res.render("admin/profile", {
+    res.render("admin/dashboard/profile", {
       user: user,
       title: "Profile",
       breadcrumb: "Profile",
@@ -28,7 +29,87 @@ async function profile(req, res) {
   }
 }
 
+async function login(req, res, next) {
+  try {
+    res.render("admin/login", { title: "Login" });
+  } catch (error) {
+    res.send("error: " + error.message);
+  }
+}
+
+async function register(req, res, next) {
+  try {
+    res.render("admin/register", { title: "Register" });
+  } catch (error) {
+    res.send("error: " + error.message);
+  }
+}
+
+async function signup(req, res, next) {
+  try {
+    const { name, username, email, phone, password, confirmpassword } =
+      req.body;
+
+    if (confirmpassword == password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await Admin.create({
+        name,
+        username,
+        email,
+        phone,
+        password: hashedPassword,
+      })
+      res.redirect('/admin/login');
+    } else {
+      $password_error = 'Password Confirmation Failed'
+      res.render({ message: $password_error });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function signin(req, res, next) {
+  const { email, password } = req.body;
+
+  const user = await Admin.findOne({
+    where: { email },
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
+
+  // Set up a session with user information
+  req.session.user = user;
+
+  // Redirect to the dashboard
+  res.redirect("/admin/dashboard");
+}
+
+async function logout(req, res, next) {
+  // Clear the user's session
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error destroying session:", err);
+    }
+    // Redirect to the login page after logout
+    res.redirect("/login");
+  });
+}
+
 module.exports = {
   index,
   profile,
+  login,
+  register,
+  signin,
+  signup,
+  logout,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const home = require('../controllers/HomeController')
-const admin = require('../controllers/AdminController')
-const middleware = require('../middlewares/authentication');
+const home = require("../controllers/HomeController");
+const admin = require("../controllers/AdminController");
+const middleware = require("../middlewares/authentication");
 
-// HomePage Routes
-router.get('/', home.homepage);
-router.get('/login', home.login);
-router.get('/register', home.register);
-router.post('/signup', home.signup);
-router.post('/signin', home.signin);
-router.get('/logout', home.logout);
+// Users Routes
+router.get("/", home.homepage);
+router.get("/users/login", home.login);
+router.get("/users/register", home.register);
+router.post("users/signup", home.signup);
+router.post("/users/signin", home.signin);
+router.get("/users/logout", home.logout);
+// Users Routes
 
-// Dashboard Routes
-router.get('/dashboard', admin.index);
-router.get('/profile', admin.profile);
+// Admin Routes
+router.get("/admin/register", admin.register);
+router.get("/admin/login", admin.login);
+router.post("/admin/signup", admin.signup);
+router.post("/admin/signin", admin.signin);
+router.get("/admin/dashboard", admin.index);
+router.get("/admin/profile", admin.profile);
+router.get("/admin/logout", admin.logout);
+// Admin Routes
+
+// Universities Routes
+
+// Universities Routes
 
 module.exports = router;
