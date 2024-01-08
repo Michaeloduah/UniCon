70a03d91c087ece60804e05ff8602537b7f06d75
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-01-08 13:30:43 +0100

    Updated login and registeration page

--- controllers/UniController.js
+++ controllers/UniController.js
@@ -1,21 +1,35 @@
 const bcrypt = require("bcrypt");
 const { University } = require("../models");
 
-async function dashboard(res, req, next) {
+async function dashboard(req, res, next) {
   try {
+    const user = req.session.user;
+
+    res.render("uni/dashboard/index", {
+      user: user,
+      title: "Dashboard",
+      breadcrumb: "Home",
+    });
   } catch (error) {
-    res.send("error " + error.message);
+    res.send("error: " + error.message);
   }
 }
 
-async function profile(res, req, next) {
+async function profile(req, res, next) {
   try {
+    const user = req.session.user;
+
+    res.render("uni/dashboard/profile", {
+      user: user,
+      title: "Profile",
+      breadcrumb: "Profile",
+    });
   } catch (error) {
-    res.send("error " + error.message);
+    res.send("error: " + error);
   }
 }
 
-async function login(res, req, next) {
+async function login(req, res, next) {
   try {
     res.render("university/login", { title: "Login" });
   } catch (error) {
@@ -23,7 +37,7 @@ async function login(res, req, next) {
   }
 }
 
-async function register(res, req, next) {
+async function register(req, res, next) {
   try {
     res.render("university/register", { title: "Register" });
   } catch (error) {
@@ -31,21 +45,34 @@ async function register(res, req, next) {
   }
 }
 
-async function signup(res, req, next) {
+async function signup(req, res, next) {
   try {
-    const { name, username, email, country, password, confirmpassword } =
-      req.body;
+    const {
+      name,
+      username,
+      email,
+      password,
+      confirmpassword,
+      country,
+      address,
+      bio,
+      account_status,
+    } = req.body;
 
     if (confirmpassword == password) {
       const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = await Admin.create({
+      const newUser = await University.create({
         name,
         username,
         email,
         phone,
         password: hashedPassword,
+        country,
+        address,
+        bio,
+        account_status,
       });
-      res.redirect("/admin/login");
+      res.redirect("/uni/login");
     } else {
       $password_error = "Password Confirmation Failed";
       res.render({ message: $password_error });
@@ -55,14 +82,31 @@ async function signup(res, req, next) {
   }
 }
 
-async function signin(res, req, next) {
-  try {
-  } catch (error) {
-    res.send("error " + error.message);
+async function signin(req, res, next) {
+  const { email, password } = req.body;
+
+  const user = await University.findOne({
+    where: { email },
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
   }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
+
+  // Set up a session with user information
+  req.session.user = user;
+
+  // Redirect to the dashboard
+  res.redirect("/uni/dashboard");
 }
 
-async function logout(res, req, next) {
+async function logout(req, res, next) {
   try {
   } catch (error) {
     res.send("error " + error.message);
